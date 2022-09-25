c2ca4957ce43654592789a69ea2413f0b6e2e323
krwi20 <kristoffwittwer@icloud.com>
2022-09-25 21:47:50 +0100

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Kris Wittwer on 25/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
