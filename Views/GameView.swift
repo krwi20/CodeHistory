61d0efed05b8b09a5923a0f05e17a76c1c311105
krwi20 <kristoffwittwer@icloud.com>
2022-09-29 03:32:50 +0100

    Integrated the GameViewModel

--- Views/GameView.swift
+++ Views/GameView.swift
@@ -16,21 +16,23 @@ struct GameView: View {
     
     /* The State property wrapper allows us to tell SwiftUI that one of the properties of our View can change. When we wrap a variable with the @State declaration, we are telling SwiftUI, “This variable can change. Update the screen if it does.” Then, when we assign a new value to any properties we have marked with @State, SwiftUI will compute the body property again to redraw the screen with the updated data. In our case, we want SwiftUI to update all of our views whenever the mainColor property changes (like when the user taps one of the buttons). */
     
-    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    @StateObject var viewModel = GameViewModel()
     
     var body: some View {
         ZStack {
-             mainColor.ignoresSafeArea() // otherwise safe areas will be default white
-             VStack {
-                 Text("1 / 10")
-                     .font(.callout)
-                     .multilineTextAlignment(.leading)
-                     .padding()
-                 Text(question.questionText) // Updated from hard-coded string
-                     .font(.largeTitle)
-                     .bold()
-                     .multilineTextAlignment(.leading)
-                 Spacer()
+            GameColor.main.ignoresSafeArea() // otherwise safe areas will be default white
+            VStack {
+                VStack {
+                Text(viewModel.questionProgressText)
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text(question.questionText)
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+            }
+            Spacer()
                  HStack {
                      
                      /* The definition starts with ForEach(). Within the parentheses, we provide the data we want to loop through. In our case, it’s a Range from 0 to the last index of possibleAnswers. Following that, we write an open curly brace and answerIndex in followed by a new line. Just like we would in a for-in loop, we give a name to the placeholder variable. Here, answerIndex will start at 0, then be assigned to 1, then 2, then 3. We can name this placeholder variable anything we like. */
@@ -38,8 +40,8 @@ struct GameView: View {
                      ForEach(0..<question.possibleAnswers.count) { answerIndex in
                          
                          Button(action: {
-                             print("Tappes on option with the text: \(question.possibleAnswers[answerIndex])")
-                             mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                             print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
+                             
                          }, label: {
                              ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
                          })
